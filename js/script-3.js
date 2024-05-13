d7bf0ccc54b9211c4327261b8c578f68e2ee6118
Ada1306 <149617245+Ada1306@users.noreply.github.com>
2024-05-13 22:21:39 +0200

    Add index.html and js

---
+++ js/script-3.js
@@ -0,0 +1,10 @@
+let a = "5";
+console.log(Number.parseInt("5"));
+// console.log(a);
+let b = "13cvb";
+console.log(Number.parseInt("13cvb"));
+// console.log(b);
+let c = "12.9sxdcfgv";
+console.log(Number.parseInt("12.9sxdcfgv"));
+// console.log(c);
+// вивести в консоль тип
---
+++ js/script-6.js
@@ -0,0 +1,3 @@
+let random = Math.random() * 2;
+random += 2;
+console.log(random);
