d2c8bcd02e2fd4fd1783bac7c6256c7d66087a4b
Ada1306 <149617245+Ada1306@users.noreply.github.com>
2024-05-23 15:17:17 +0200

    Delete script-3.js

--- script-3.js
+++
@@ -1,9 +0,0 @@
-let a = "5";
-console.log(Number.parseInt("5"));
-// console.log(typeof Number (a);
-let b = "13cvb";
-console.log(Number.parseInt("13cvb"));
-// console.log(typeof parseInt(b);
-let c = "12.9sxdcfgv";
-console.log(Number.parseInt("12.9sxdcfgv"));
-// console.log(typeof parseFloat)(c);
